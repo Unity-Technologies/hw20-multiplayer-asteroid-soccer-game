a1b51d22d4d80e18801901e0b4880c281a4291a8
Matt Brown <matthew.brown@unity3d.com>
2020-06-23 15:32:04 -0500

    incremental progress getting matchcommunicationmanager in

--- FootRoidsClient/Assets/Scripts/Multiplayer/MatchCommunicationManager.cs
+++ FootRoidsClient/Assets/Scripts/Multiplayer/MatchCommunicationManager.cs
@@ -1,18 +1,105 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Nakama;
+using Nakama.TinyJson;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
-public class MatchCommunicationManager : MonoBehaviour
+namespace Multiplayer
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MatchCommunicationManager : Singleton<MatchCommunicationManager>
     {
-        
-    }
+        [SerializeField] private int _playerCount = 2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public event Action OnGameStarted;
+        public event Action<MatchMessageGameEnded> OnGameEnded;
+
+        public string CurrentHostId { private set; get; }
+
+        public bool IsHost
+        {
+            get
+            {
+                return CurrentHostId == NakamaSessionManager.Instance.Session.UserId;
+            }
+        }
+
+        public List<IUserPresence> Players { private set; get; }
+
+        public bool AllPlayersJoined {  get { return Players.Count == _playerCount;  } }
+
+        public bool GameStarted { private set; get; }
+
+        private ISocket _socket { get { return NakamaSessionManager.Instance.Socket;  } }
+
+        private bool allPlayersAdded;
+        private bool matchJoined;
+        private bool isLeaving;
+        private Queue<>
+
+        private void Start()
+        {
+            OnGameEnded += GameEnded;
+        }
+
+        private void StartGame()
+        {
+            if(GameStarted == true)
+            {
+                return;
+            }
+            if(allPlayersAdded == false || matchJoined == false)
+            {
+                return;
+            }
+            GameStarted = true;
+
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                Debug.Log("Starting Game!");
+                OnGameStarted?.Invoke();
+                while(inboundMessages.Count > 0)
+                {
+
+                }
+            });            
+        }
+
+        protected override void OnDestroy()
+        {
+            Debug.Log("Implement me!!!");
+   
+        }
+
+        private void GameEnded(MatchMessageGameEnded obj)
+        {
+            _socket.ReceivedMatchPresence -= OnMatchPresence;
+        }
+
+        private void OnMatchPresence(IMatchPresenceEvent e)
+        {
+            foreach(IUserPresence user in e.Joins)
+            {
+                if (Players.FindIndex(x => x.UserId == user.UserId) == -1)
+                {
+                    Debug.Log("User " + user.Username + " joined match");
+                    Players.Add(user);
+                    // NOTE: not sure how we are handling opponent logic yet
+                    //if (user.UserId != NakamaSessionManager.Instance.Session.UserId)
+                    //{
+                    //    OpponentId = user.UserId;
+                    //}
+                    if (AllPlayersJoined == true)
+                    {
+                        allPlayersAdded = true;
+                        StartGame();
+                    }
+                }
+            }
+        }
     }
 }
+
