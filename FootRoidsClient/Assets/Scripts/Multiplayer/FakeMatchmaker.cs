d7ec9047e5f30155bd29788910aa0b3f9bb6440b
Marc-Étienne Lebeau <marcetienne.lebeau@unity3d.com>
2020-06-23 17:13:54 -0400

    Fake matchmaking scene and trimmed down NakamaSessionManager called ServerSessionManager

---
+++ FootRoidsClient/Assets/Scripts/Multiplayer/FakeMatchmaker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using Multiplayer;
+using UnityEngine;
+
+using Nakama;
+using Nakama.TinyJson;
+
+namespace FootRoids
+{
+    [Serializable]
+    public struct FakeUser
+    {
+        public string email;
+        public string password;
+    }
+
+    public class FakeMatchmaker : MonoBehaviour
+    {
+        [SerializeField] FakeUser[] m_Users = null;
+        readonly IClient m_Client = new Client("http", "207.254.17.33", 7350, "defaultkey");
+        List<ISocket> m_Sockets = new List<ISocket>();
+        IMatch m_Match;
+
+        async void Start()
+        {
+            foreach (var user in m_Users)
+            {
+                ServerSessionManager.Instance.Session = await m_Client.AuthenticateEmailAsync(user.email, user.password);
+                ServerSessionManager.Instance.Socket = m_Client.NewSocket();
+
+                ServerSessionManager.Instance.Socket.Connected += () => { Debug.Log("Socket Connected!"); };
+
+                ServerSessionManager.Instance.Socket.Closed += () => { Debug.Log("Socket Closed!"); };
+
+                ServerSessionManager.Instance.Socket.ReceivedChannelMessage += (message) => { Debug.Log("Message Received: " + message); };
+
+                ServerSessionManager.Instance.Socket.ReceivedMatchmakerMatched += async (matched) =>
+                {
+                    Debug.Log("Matched!");
+                    var match = await ServerSessionManager.Instance.Socket.JoinMatchAsync(matched);
+                    if (m_Match == null)
+                    {
+                        m_Match = match;
+                    }
+                    
+                    
+                    /*UnityMainThreadDispatcher.Instance().Enqueue(() =>
+                    {
+                        ISocket socket = NakamaSessionManager.Instance.Socket;
+                        socket.ReceivedMatchmakerMatched -= OnMatchmakerMatched;
+
+                        StartCoroutine(LoadBattle(e));
+                    });*/
+                    
+                    
+                    
+                    
+                };
+
+                ServerSessionManager.Instance.Socket.ReceivedMatchState += async (state) =>
+                {
+                    Debug.Log("Received Message From Server: " + state.State);
+                };
+
+                await ServerSessionManager.Instance.Socket.ConnectAsync(ServerSessionManager.Instance.Session);
+                await ServerSessionManager.Instance.Socket.AddMatchmakerAsync("*", 2, 2);
+                m_Sockets.Add(ServerSessionManager.Instance.Socket);
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            foreach (var socket in m_Sockets)
+            {
+                socket?.CloseAsync();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                if (m_Match == null)
+                {
+                    Debug.Log("Match is null");
+                }
+
+                Debug.Log("Sending Message To Server!");
+                var socket = m_Sockets[0];
+                var newState = new Dictionary<string, string> {{"hello", "world"}}.ToJson();
+                socket.SendMatchStateAsync(m_Match.Id, 1, newState);
+            }
+        }
+    }
+}
---
+++ FootRoidsClient/Assets/Scripts/Multiplayer/Session/ServerSessionManager.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using UnityEngine;
+
+using Nakama;
+
+namespace Multiplayer
+{
+    public class ServerSessionManager : Singleton<ServerSessionManager>
+    {
+        [SerializeField] string _serverAddress = "207.254.17.33";
+        [SerializeField] int _port = 7350;
+        
+        string _deviceId;
+
+        /// <summary>
+        /// Used to establish connection between the client and the server.
+        /// Contains a list of usefull methods required to communicate with Nakama server.
+        /// Do not use this directly, use <see cref="Client"/> instead.
+        /// </summary>
+        Client _client;
+
+        /// <summary>
+        /// Socket responsible for maintaining connection with Nakama server and exchanger realtime messages.
+        /// Do not use this directly, use <see cref="Socket"/> instead.
+        /// </summary>
+        ISocket _socket;
+
+        /// <summary>
+        /// Used to communicate with Nakama server.
+        /// For the user to send and receive messages from the server, <see cref="Session"/> must not be expired.
+        /// Default expiration time is 60s, but for this demo we set it to 3 weeks (1 814 400 seconds).
+        /// To initialize the session, call <see cref="AuthenticateDeviceIdAsync"/> or <see cref="AuthenticateFacebookAsync"/> methods.
+        /// To reinitialize expired session, call <see cref="Reauthenticate"/> method.
+        /// </summary>
+        public ISession Session { get; set; }
+        
+        /// <summary>
+        /// Contains all the identifying data of a <see cref="Client"/>, like User Id, linked Device IDs,
+        /// linked Facebook account, username, etc.
+        /// </summary>
+        public IApiAccount Account { get; set; }
+
+        /// <summary>
+        /// Used to establish connection between the client and the server.
+        /// Contains a list of usefull methods required to communicate with Nakama server.
+        /// </summary>
+        public Client Client => _client ?? (_client = new Client("http", _serverAddress, _port, "defaultkey", UnityWebRequestAdapter.Instance));
+
+        /// <summary>
+        /// Socket responsible for maintaining connection with Nakama server and exchange realtime messages.
+        /// </summary>
+        public ISocket Socket
+        {
+            get
+            {
+                if (_socket == null)
+                {
+                    // Initializing socket
+                    _socket = _client.NewSocket();
+                }
+                
+                return _socket;
+            }
+
+            set => _socket = value;
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                if (Session == null || Session.HasExpired(DateTime.UtcNow) == true)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            GetDeviceId();
+        }
+
+        /// <summary>
+        /// Closes Nakama session.
+        /// </summary>
+        protected override void OnDestroy()
+        {
+            Disconnect();
+        }
+
+        public void SetIp(string ip)
+        {
+            if (IsConnected == false)
+            {
+                _serverAddress = ip;
+            }
+        }
+
+        /// <summary>
+        /// Removes session and account from cache, logs out of Facebook and invokes <see cref="OnDisconnected"/>.
+        /// </summary>
+        public void Disconnect()
+        {
+            if (Session != null)
+            {
+                Session = null;
+                Account = null;
+
+                Debug.Log("Disconnected from Nakama");
+            }
+        }
+
+        /// <summary>
+        /// Retrieves device id from player prefs. If it's the first time running this app
+        /// on this device, <see cref="_deviceId"/> is filled with <see cref="SystemInfo.deviceUniqueIdentifier"/>.
+        /// </summary>
+        private void GetDeviceId()
+        {
+            if (string.IsNullOrEmpty(_deviceId) == true)
+            {
+                _deviceId = PlayerPrefs.GetString("nakama.deviceId");
+                if (string.IsNullOrWhiteSpace(_deviceId) == true)
+                {
+                    // SystemInfo.deviceUniqueIdentifier is not supported in WebGL,
+                    // we generate a random one instead via System.Guid
+#if UNITY_WEBGL && !UNITY_EDITOR
+                    _deviceId = System.Guid.NewGuid().ToString();
+#else
+                    _deviceId = SystemInfo.deviceUniqueIdentifier;
+#endif                    
+                    PlayerPrefs.SetString("nakama.deviceId", _deviceId);
+                }
+            }
+        }
+    }
+}
