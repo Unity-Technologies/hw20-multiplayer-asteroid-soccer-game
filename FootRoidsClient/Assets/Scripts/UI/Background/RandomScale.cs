0d45cd82fc8735d92f5b01e50f2b572ee26692c0
Danny Dyer <danny@unity3d.com>
2020-06-23 20:21:19 -0500

    WIP: setting up a main menu / login flow

---
+++ FootRoidsClient/Assets/Scripts/UI/Background/RandomScale.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScale : MonoBehaviour
+{
+    public float MinScale = 1.0f;
+    public float MaxScale = 2.0f;
+
+    void Start() {
+        transform.localScale = transform.localScale * Random.Range(MinScale, MaxScale);
+    }
+}
---
+++ FootRoidsClient/Assets/Scripts/UI/Background/RandomTorque.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTorque : MonoBehaviour
+{
+    public float MinTorque = 1.0f;
+    public float MaxTorque = 10.0f;
+
+	public void Start() {
+        var randomTorque = Random.Range(MinTorque, MaxTorque);
+        GetComponent<Rigidbody2D>().AddTorque(randomTorque);
+	}
+
+}
