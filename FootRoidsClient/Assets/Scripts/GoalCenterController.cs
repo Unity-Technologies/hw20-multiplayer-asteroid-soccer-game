23de4c8ebde8cc60189da7e2da460421a38d1a1a
Jennifer Tam <jennifert@unity3d.com>
2020-06-24 16:16:49 -0700

    added collision score tracking logic

--- FootRoidsClient/Assets/Scripts/GoalCenterController.cs
+++ FootRoidsClient/Assets/Scripts/GoalCenterController.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class GoalCenterController : MonoBehaviour
 {
+    static int goalScore = 0;
     // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collision)
+    {   
+        Debug.Log("goalCenter trigger");
+        if (collision.gameObject.tag.Equals("Ball"))
+        {
+            ScoreScript.scoreValue += 1;
+            Destroy (collision.gameObject); // Destroy the colliding socccer ball
+            Destroy(gameObject); // Destroy the goal...probably don't want to destroy this
+            Debug.Log($"score: {ScoreScript.scoreValue}");
+        }
+    }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
