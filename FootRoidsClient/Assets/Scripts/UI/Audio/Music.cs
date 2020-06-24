f9441714971d82c9c0473ee663cf9ea3584f78ff
Lauryn Porte <lauryn@unity3d.com>
2020-06-24 14:10:10 -0700

    Add audio button assets and background music to menu

---
+++ FootRoidsClient/Assets/Scripts/UI/Audio/Music.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour
+{
+	static Music instance = null;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+        }
+		else
+		{
+            // Allow audio to keep playing between scenes
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+    }
+
+	public void ToggleSound()
+	{
+		if (PlayerPrefs.GetInt("Muted", 0) == 0)
+		{
+            // Turn music on
+			PlayerPrefs.SetInt("Muted", 1);
+		}
+		else
+		{
+			// Turn music off
+			PlayerPrefs.SetInt("Muted", 0);
+		}
+    }
+}
