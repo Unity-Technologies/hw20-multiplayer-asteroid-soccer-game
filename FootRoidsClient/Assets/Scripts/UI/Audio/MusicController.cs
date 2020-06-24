e5372ee331b87a78c22859c54e7b7021835ad69f
Lauryn Porte <lauryn@unity3d.com>
2020-06-24 14:36:09 -0700

    Add music controller

--- FootRoidsClient/Assets/Scripts/UI/Audio/MusicController.cs
+++ FootRoidsClient/Assets/Scripts/UI/Audio/MusicController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Music music;
+    public Button musicToggleButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+
+    private void Start()
     {
-        
+        music = FindObjectOfType<Music>();
+        UpdateIconAndSound();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StopMusic()
     {
-        
+        music.ToggleSound();
+        UpdateIconAndSound();
+    }
+
+    private void UpdateIconAndSound()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            musicToggleButton.GetComponent<Image>().sprite = musicOnSprite;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            musicToggleButton.GetComponent<Image>().sprite = musicOffSprite;
+        }
     }
 }
