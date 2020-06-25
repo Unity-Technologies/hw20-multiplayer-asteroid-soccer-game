064bcea79af1ff5e8aaefa3c204327e81ad1901f
Alex Garcia <alexg@unity3d.com>
2020-06-25 15:13:28 -0500

    added more dummies

--- FootRoidsClient/Assets/Scripts/DummyController.cs
+++ FootRoidsClient/Assets/Scripts/DummyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dummycontroller : MonoBehaviour
+public class DummyController : MonoBehaviour
 {
    
     // Start is called before the first frame update
