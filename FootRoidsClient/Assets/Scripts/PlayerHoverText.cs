405df5451cfdde8ba9e82aee3680343940101a89
Jennifer Tam <jennifert@unity3d.com>
2020-06-25 11:49:12 -0700

    got player text to show hovered above player game object

---
+++ FootRoidsClient/Assets/Scripts/PlayerHoverText.cs
@@ -0,0 +1,25 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//// TODO: get this script working to show usernames from client.
+//public class PlayerHoverText : MonoBehaviour
+//{
+//    public static string playerUsername = "new-user";
+//    Text player;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        player = GetComponent<TMPro.TextMeshProUGUI>().text;
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        player.text = $"Player Username: {playerUsername}";
+//    }
+//}
+
