75de8cab0c18508546b1091a9f4245029eae1490
Lauryn Porte <lauryn@unity3d.com>
2020-06-24 20:53:41 -0700

    Add scene trainsition to main game; Lower volume of background music; Update builds to have scenes, add foot asset; Add placeholder NPC dir

--- FootRoidsClient/Assets/Scripts/UI/MainMenu.cs
+++ FootRoidsClient/Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,9 @@
 using System.Threading.Tasks;
 using UnityEngine;
 using Nakama;
+using System.Collections.Generic;
+using Nakama.TinyJson;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,12 +13,14 @@ public class MainMenu : MonoBehaviour
     public GameObject MatchmakingPanel;
     public UnityEngine.UI.Text UsernameInputText;
     public UnityEngine.UI.Text UsernameDisplayText;
+    public UnityEngine.UI.Text ChatroomText; // TODO
 
     // TODO: move all the Nakama stuff somewhere else
-    private readonly IClient _client = new Client("http", "127.0.0.1", 7350, "defaultkey");
+    private readonly IClient _client = new Client("http", "35.188.32.254", 7350, "defaultkey");
     private ISession _session;
     private ISocket _socket;
     private IMatch _match;
+    private const string RoomName = "Cool Chat";
 
     public async void ButtonPressPlay() {
         // show loading
@@ -38,6 +43,8 @@ public class MainMenu : MonoBehaviour
         UsernamePanel.SetActive(false);
 
         // start multiplayer session
+        Debug.Log("ButtonPressUsernameAccept: session? ");
+        Debug.Log(_session);
         _session = await startSession(UsernameInputText.text);
 
         // go to next panel
@@ -74,7 +81,7 @@ public class MainMenu : MonoBehaviour
         }
         catch (Exception e)
         {
-            Debug.LogError("Counldn't connect to Nakama server; message: " + e);
+            Debug.LogError("Couldn't connect to Nakama server; message: " + e);
         }
     }
 
@@ -95,19 +102,30 @@ public class MainMenu : MonoBehaviour
     }
 
     async void startMatchmaking() {
+        //Socket Handlers
         _socket = _client.NewSocket();
-        _socket.Connected += () => {
+        _socket.Connected += () =>
+        {
             Debug.Log("Socket Connected!");
         };
-        _socket.Closed += () => {
+        _socket.Closed += () =>
+        {
             Debug.Log("Socket Closed!");
         };
-        _socket.ReceivedChannelMessage += (message) => {
+        _socket.ReceivedChannelMessage += (message) =>
+        {
             Debug.Log("Message Received: " + message);
         };
-        _socket.ReceivedMatchmakerMatched += async (matched) => {
+        _socket.ReceivedMatchmakerMatched += async (matched) =>
+        {
             Debug.Log("Matched!");
             var match = await _socket.JoinMatchAsync(matched);
+            Debug.LogFormat("Match? {0}", match);
+
+            var self = match.Self;
+            Debug.LogFormat("Self: {0}", self);
+            Debug.LogFormat("Presences: {0}", match.Presences);
+
             if (_match == null)
             {
                 _match = match;
@@ -115,13 +133,35 @@ public class MainMenu : MonoBehaviour
 
             // TODO: update some UI
 
-            foreach (var presence in _match.Presences) {
-                Debug.Log("presence: "+presence.Username);
+            foreach (var presence in _match.Presences)
+            {
+                Debug.Log("presence: " + presence.Username);
             }
+
+            // Transition to Game
+            SceneManager.LoadScene("MainGame");
+
+
         };
 
         await _socket.ConnectAsync(_session);
-        await _socket.AddMatchmakerAsync("*", 2, 2);
+        await _socket.AddMatchmakerAsync("*", 2, 9);
+
+        var channel = await _socket.JoinChatAsync(RoomName, ChannelType.Room);
+        Debug.LogFormat("Join chat channel: {0}", channel);
+
+        var content = new Dictionary<string, string> { { "hello", "world" } }.ToJson();
+        _ = _socket.WriteChatMessageAsync(channel, content);
+
+        // Fake another user to test matchmaking
+        //var deviceId2 = Guid.NewGuid().ToString();
+        //var session2 = await _client.AuthenticateDeviceAsync(deviceId2);
+        //var socket2 = _client.NewSocket();
+        //socket2.ReceivedMatchmakerMatched += async matched => await socket2.JoinMatchAsync(matched);
+        //await socket2.ConnectAsync(session2);
+        //await socket2.AddMatchmakerAsync("*", 2, 9);
+        //await Task.Delay(TimeSpan.FromSeconds(120));
+        //await socket2.CloseAsync();
     }
 
     private void OnApplicationQuit()
