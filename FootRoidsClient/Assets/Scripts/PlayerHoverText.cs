8a306dacbd5ae9ba483c38dc8c17c08c3d582f39
Danny Dyer <danny@unity3d.com>
2020-06-25 14:35:34 -0500

    constrain rotation of player text

--- FootRoidsClient/Assets/Scripts/PlayerHoverText.cs
+++ FootRoidsClient/Assets/Scripts/PlayerHoverText.cs
@@ -1,25 +1,19 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-//// TODO: get this script working to show usernames from client.
-//public class PlayerHoverText : MonoBehaviour
-//{
-//    public static string playerUsername = "new-user";
-//    Text player;
+// TODO: get this script working to show usernames from client.
+public class PlayerHoverText : MonoBehaviour
+{
+    public static string playerUsername = "new-user";
+    Text player;
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        player = GetComponent<TMPro.TextMeshProUGUI>().text;
-        
-//    }
+    void Start()
+    {
+        //player = GetComponent<TMPro.TextMeshProUGUI>().text;
+        //player.text = $"Player Username: {playerUsername}";
+    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        player.text = $"Player Username: {playerUsername}";
-//    }
-//}
+}
 
