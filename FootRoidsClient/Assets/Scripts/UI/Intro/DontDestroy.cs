5ae52970a84d794266b60305ae8a60e6d4dc82d1
Lauryn Porte <lauryn@unity3d.com>
2020-06-25 17:15:35 -0700

    Add intro scene

---
+++ FootRoidsClient/Assets/Scripts/UI/Intro/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("AudioButton");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+}
---
+++ FootRoidsClient/Assets/Scripts/UI/Intro/SplashFade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashFade : MonoBehaviour
+{
+    public Image splashImage;
+    public Text splashText;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        // Fades image and text
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+        splashText.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(4.5f);
+        FadeOut();
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 3.0f, false);
+        splashText.CrossFadeAlpha(1.0f, 3.0f, false);
+    }
+
+    void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 3.0f, false);
+        splashText.CrossFadeAlpha(0.0f, 3.0f, false);
+    }
+
+
+}
